7d8efb572dcef47d8f48b8e79f21a36a58d22449
Gabriel DP <gabrieldominguezprado@gmail.com>
2021-12-15 14:42:43 -0300

    Segundo Commit

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -11,9 +11,10 @@ import { AuthService } from '../../services/auth.service';
 
     `
     .card-fondo{
-    background: rgba(0,0,0,0.2) !important;
-    box-shadow: 0px 0px 4px 5px rgba(0.21, 0.21, 0.21, 0.21), 0px 0px 4px 4px rgba(0.24, 0.24, 0.24, 0.24) !important;
-}
+      
+      background: rgba(0,0,0,0.2) !important;
+      box-shadow: 0px 0px 4px 5px rgba(0.21, 0.21, 0.21, 0.21), 0px 0px 4px 4px rgba(0.24, 0.24, 0.24, 0.24) !important;
+    }
     `
   ]
 })
